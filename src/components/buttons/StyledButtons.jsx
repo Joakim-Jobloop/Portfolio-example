868e392df6ea9df17487b839a4650a8fd17c4899
JoakimJobloop <Joakim@jobloop.no>
2024-01-24 10:58:17 +0100

    *

---
+++ src/components/buttons/StyledButtons.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StandardButton = styled.button`
+  background-color: yellow;
+  color: black;
+  font-size: 16px;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  margin: 10px;
+`;
--- src/pages/HomePageSections/HeroSection.jsx
+++ src/pages/HomePageSections/HeroSection.jsx
@@ -1,10 +1,13 @@
+import { StandardButton } from "../../components/buttons/StyledButtons";
+
 export const HeroSection = () => {
-    return (
-        <section className="w-full h-[calc(100dvh-96px)]">
-            <div className="w-full h-full flex flex-col justify-center items-center">
-                <h1 className="text-5xl font-bold">Villo</h1>
-                <p className="text-2xl font-bold">Utvikling</p>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="w-full h-[calc(100dvh-96px)]">
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <h1 className="text-5xl font-bold">Villo</h1>
+        <p className="text-2xl font-bold">Utvikling</p>
+        <StandardButton>Click me!</StandardButton>
+      </div>
+    </section>
+  );
+};
